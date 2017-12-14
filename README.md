c37f0dcb4be45447c85bbd194b227629bdab74ec
Nicolas Hefti <nicolas@hefti.fr>
2017-12-14 08:33:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Appizy App Embed
+
+Appizy official WordPress plugin is the easiest way to embed the web-calculator created with Appizy into your content.
+
+Note: this is the development area. For the official plugin package, got to the office WordPress page.
+
+## Usage
+
+## Contribute
+
+### Install
+
+The development of the plugin is Dockerified for easy setup.
+
+Step 1 - start Docker
+
+```bash
+docker-compose -f ./docker/docker-compose.yml up
+```
+
+Step 2 - visite your localhost:8080 in your browser and the WordPress setup
+
+Step 3 - activate the plugin in the WordPress interface.
+
+Step 4 - enjoy testing, developing...
+
+
---
+++ src/hello-molly.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package Hello_Molly
+ * @version 1.0
+ */
+
+/*
+Plugin Name: Hello Molly
+Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
+Author: Nicolas Hefti
+Version: 1.0
+*/
+
+class MyPlugin
+{
+    public static function baztag_func($atts, $content = "")
+    {
+        return "content = $content";
+    }
+}
+
+add_shortcode('appi-me', array('MyPlugin', 'baztag_func'));
+
+?>
