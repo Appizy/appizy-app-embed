ea5ca48c877d6b2710e3efbc140e543f8edc2828
Nicolas Hefti <nicolas@hefti.fr>
2017-12-14 16:52:23 +0100

    CS check

--- src/hello-molly.php
+++ src/hello-molly.php
@@ -4,21 +4,23 @@
  * @version 1.0
  */
 
+namespace Appizy;
+
 /*
 Plugin Name: Hello Molly
-Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
+Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in
+    two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from
+    <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
 Author: Nicolas Hefti
 Version: 1.0
 */
 
 class MyPlugin
 {
-    public static function baztag_func($atts, $content = "")
+    public static function fooFunc($atts, $content = "")
     {
         return "content = $content";
     }
 }
 
-add_shortcode('appi-me', array('MyPlugin', 'baztag_func'));
-
-?>
+add_shortcode('appi-me', ['MyPlugin', 'fooFunc']));
